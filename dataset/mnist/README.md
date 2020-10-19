bccaef9c42450287a7b8984d039b5a81e7b4ec45
jameslimer <jameslimers@gmail.com>
2020-10-19 16:14:57 +0800

    add mnist process

--- dataset/mnist/README.md
+++ dataset/mnist/README.md
@@ -7,5 +7,18 @@ python ./dataset/mnist/download_mnist.py -d ./dataset/mnist/raw_data/
 ```
 - Extract dataset file (delete the original '.gz' file):
 ```
-python ./dataset/mnist/extract_mnist.py -d ./dataset/mnist/raw_data/ -r True
+python ./dataset/mnist/extract_mnist.py -d ./dataset/mnist/raw_data/ --remove True
+```
+- Process the dataset file:
+```
+python ./dataset/mnist/process_mnist.py -i ./dataset/mnist/raw_data/ -o ./dataset/mnist/processed_data/
+```
+
+- After run above code, the mnist processed_data folder will contail following file:
+```
+.
+├─mnist
+└─ processed code
+    ├── mnist_test.pt
+    └── mnist_train.pt
 ```
---
+++ dataset/mnist/process_mnist.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import argparse
+import torch
+from dataset.mnist.utils_mnist import read_image_file, read_label_file
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input_dir", type=str, help="Directory that original data in", required=True)
+parser.add_argument("-o", "--output_dir", type=str, help="Directory that target data to put", required=True)
+args = parser.parse_args()
+
+raw_folder = args.input_dir
+processed_folder = args.output_dir
+
+
+training_file = "mnist_train.pt"
+test_file = "mnist_test.pt"
+
+print('Processing...')
+
+training_set = (
+    read_image_file(os.path.join(raw_folder, 'train-images-idx3-ubyte')),
+    read_label_file(os.path.join(raw_folder, 'train-labels-idx1-ubyte'))
+)
+test_set = (
+    read_image_file(os.path.join(raw_folder, 't10k-images-idx3-ubyte')),
+    read_label_file(os.path.join(raw_folder, 't10k-labels-idx1-ubyte'))
+)
+
+with open(os.path.join(processed_folder, training_file), 'wb') as f:
+    torch.save(training_set, f)
+with open(os.path.join(processed_folder, test_file), 'wb') as f:
+    torch.save(test_set, f)
+
